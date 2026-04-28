fb5891c12207beea008f5a673466e89fe8bc04b7
michael <michaelleung0417@gmail.com>
2026-04-28 21:25:23 +0800

    basic lucky draw layout

--- app/src/main/java/com/example/luckydraw/MainActivity.kt
+++ app/src/main/java/com/example/luckydraw/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.luckydraw
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    var isSpinning = false
+
+    fun disablePlayButton() {
+        var playButtonTV = findViewById<Button>(R.id.playTV)
+        playButtonTV.isEnabled = false
+    }
+
+    fun enablePlayButton() {
+        var playButtonTV = findViewById<Button>(R.id.playTV)
+        playButtonTV.isEnabled = true
+    }
+
+    fun play(view: View) {
+        if(isSpinning) return
+
+        isSpinning = true
+        disablePlayButton()
+        var randomNumber = (1..10).random()
+        var luckyNumberTV = findViewById<TextView>(R.id.luckyNumberTV)
+        luckyNumberTV.text = randomNumber.toString()
+        isSpinning = false
+        enablePlayButton()
     }
 }
